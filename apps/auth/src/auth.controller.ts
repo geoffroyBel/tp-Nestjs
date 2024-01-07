4117d1db9c8dd9a30dd003c9ce38abfd7dc05caf
geoff <froygeo@live.fr>
2024-01-07 18:43:00 +0100

    status code ok

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { LocalAuthGuard } from './guards/local-auth.guards';
@@ -12,6 +20,7 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('/signin')
+  @HttpCode(200)
   async signin(
     @currentUser() user: User,
     @Res({ passthrough: true }) response: Response,
